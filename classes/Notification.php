714412b417523d4c98113e95915b274c39d1aedd
Shawn Patrick Rice <rice@shawnrice.org>
2015-02-14 00:53:49 -0500

    updated tests

--- classes/Notification.php
+++ classes/Notification.php
@@ -4,20 +4,11 @@ namespace Alphred;
 
 class Notification {
 
-	// The notification will always have the script editor icon on it.
-	// Use CocoaDialog for better notifications.
-	public function notify( $options ) {
-		if ( is_string( $options ) ) {
-			exec( "osascript -e 'display notification \"{$options}\"'" );
-			return true;
-		}
-		if ( ! isset( $options['text'] ) ) {
-			// throw exception
-			return false;
-		}
-
-		// These are the available, built-in sounds that you can use
-		$sounds = [
+/**
+ * The available, built-in sounds that you can use
+ * @var array
+ */
+private static $sounds = [
 				'Basso',
 				'Bottle',
 				'Funk',
@@ -34,6 +25,28 @@ class Notification {
 				'Tink'
 		];
 
+
+
+
+	/**
+	 * Sends a system notification
+	 *
+	 * The notification will always have the script editor icon on it.
+	 * Use CocoaDialog for better notifications.
+	 *
+	 * @param  string|array $options [description]
+	 * @return boolean          [description]
+	 */
+	public function notify( $options ) {
+		if ( is_string( $options ) ) {
+			exec( "osascript -e 'display notification \"{$options}\"'" );
+			return true;
+		}
+		if ( ! isset( $options['text'] ) ) {
+			// throw exception
+			return false;
+		}
+
 		$script = "osascript -e 'display notification \"{$options['text']}\"";
 		foreach ( $options as $field => $option ) :
 			switch ( $field ) :
@@ -44,7 +57,7 @@ class Notification {
 				$script .= " subtitle \"{$option}\"";
 					break;
 			case 'sound' :
-				if ( in_array( $option, $sounds ) ) {
+				if ( in_array( $option, self::$sounds ) ) {
 					$script .= " sound name \"{$option}\"";
 				}
 				break;
--- tests/NotificationTest.php
+++ tests/NotificationTest.php
@@ -6,7 +6,11 @@ require_once( 'setup.php' );
 class NotificationTest extends \PHPUnit_Framework_TestCase {
 
 	public function test_notify() {
-		Alphred\Notification::notify(['text' => 'This is a test notification', 'title' => 'Test Notification' ] );
+		Alphred\Notification::notify(['text' => 'This is a test notification', 'title' => 'Test Notification', 'sound' => 'Purr', 'subtitle' => 'this is a subtitle' ] );
+	}
+
+	public function test_notify2() {
+		Alphred\Notification::notify( 'This is a test notification' );
 	}
 
 }
